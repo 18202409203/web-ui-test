538b27a3090e5d5b4d386f57f897bb67147d1d47
TK Zhang <435773573@qq.com>
2018-04-27 15:23:50 +0800

    Add files via upload

--- src/components/timelines.vue
+++ src/components/timelines.vue
@@ -15,34 +15,44 @@ export default {
         return {
             ip: 'http://219.216.80.110:8080',
             rawData: [],
+            //故障类型根据紧急程度由高到低排序
             categoryLabels: [
-                {stopCode: '1000', stopName: '启动'},
-                {stopCode: '0001', stopName: '机械故障'},
-                {stopCode: '0002', stopName: '电气故障'},
-                {stopCode: '0003', stopName: '生产故障'},
-                {stopCode: '0004', stopName: '停紧'},
-                {stopCode: '0005', stopName: '待开'},
-                {stopCode: '0006', stopName: '计划停'},
-                {stopCode: '0007', stopName: '烘炉'},
-                {stopCode: '0008', stopName: '待料'},
+
+                {stopCode: '1000', stopName: '运行'},
+
                 {stopCode: '0009', stopName: '限开'},
-                {stopCode: '0010', stopName: '检修待开'},
-                {stopCode: '0011', stopName: '计划待开'},
-                {stopCode: '0012', stopName: '停产'},
+                {stopCode: '0008', stopName: '待料'},
+                {stopCode: '0005', stopName: '待开'},
                 {stopCode: '0013', stopName: '待煤气'},
+                {stopCode: '0010', stopName: '检修待开'},
+
+                {stopCode: '0011', stopName: '计划待开'},
+                {stopCode: '0018', stopName: '计划定修'},
+                {stopCode: '0006', stopName: '计划停'},
+                {stopCode: '0017', stopName: '大中修'},
+                {stopCode: '0016', stopName: '检修'},
+                {stopCode: '0012', stopName: '停产'},
+
+
+                {stopCode: '0007', stopName: '烘炉'},
                 {stopCode: '0014', stopName: '淌炉压炉'},
                 {stopCode: '0015', stopName: '烟机震动'},
-                {stopCode: '0016', stopName: '检修'},
-                {stopCode: '0017', stopName: '大中修'},
-                {stopCode: '0018', stopName: '计划定修'},
+
+        
                 {stopCode: '0019', stopName: '其他'},
-                {stopCode: '0020', stopName: '未知故障'}
+                {stopCode: '0004', stopName: '停紧'}, 
+                {stopCode: '0020', stopName: '未知故障'},
+                {stopCode: '0001', stopName: '机械故障'},
+                {stopCode: '0002', stopName: '电气故障'},
+                {stopCode: '0003', stopName: '生产故障'}
+
             ]
         }
     },
     methods:{
         fetchData() {
-            axios.get(this.ip + '/lyj/stoptime/getEquipTimeLineByDateScopAndProcedureid/2017-1-1/2017-6-1/all').then(response => {
+            let vm = this
+            axios.get(this.ip + '/lyj/stoptime/getEquipTimeLineByDateScopAndProcedureid/2017-1-1/2017-2-1/all').then(response => {
                 if(response.status === 200) {
                     this.rawData = response.data
                     let data = d3.nest()
@@ -62,9 +72,10 @@ export default {
                         .zColorScale(
                             d3.scaleOrdinal() // 按顺序依次对应 停歇原因 和 颜色
                                 .domain(function(){
-                                    return this.categoryLabels.map( (d, i)=> d.stopName)
-                                })
-                                .range([...d3.schemeCategory10,...d3.schemePaired])
+                                    return vm.categoryLabels.map( (d, i)=> d.stopName)
+                                }())
+                                .range(["#2962FF",...d3.schemeBlues[7].slice(2),...d3.schemeGreens[8].slice(2),"#42A5F5",
+                                "#FFA726","#FF9100",...d3.schemeReds[9].slice(3)])
                         )
                         //********************************************//
                         (document.getElementById('timelines'));
